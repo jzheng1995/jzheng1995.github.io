e001759a97babe7c127a8a2e17ed358feb097622
jzheng1995 <jzheng1995@users.noreply.github.com>
2024-03-13 06:56:05 +0000

    deploy: c58fcf57126e9d51726c64e639e6da02b28dccd2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Home Credit 01: ETL",
+        "excerpt":"2024-03-11-home_credit_ETL.knit Code Show All Code Hide All Code 0.1 Introduction This is primarily an ETL document for the 2024 home credit competition data on Kaggle. This will also be a first attempt at 1) a kaggle competition, 2) approaching this dataset, and 3) documenting the bottom-up process for public review....","categories": [],
+        "tags": [],
+        "url": "/homecredit/01-home-credit-etl/",
+        "teaser": null
+      },{
+        "title": "First post",
+        "excerpt":"This is my first post! This is a blog post built with Jekyll and Markdown, hosted on Github. Let’s see what we can do. Syntax We can make headings! Heading 1 Heading 2 Heading 3 Heading 4 Text can be bold, italicized, or both. We can also make block quotes....","categories": [],
+        "tags": [],
+        "url": "/2024/03/10/first-post/",
+        "teaser": null
+      }]
